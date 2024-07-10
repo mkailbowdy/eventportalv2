796f214eff8933c3f1d5a2e908d96a69adbe290c
Myhkail Mendoza <myhkail.mendoza@gmail.com>
2024-07-10 14:54:34 +0900

    user policy

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,5 @@
 <?php
 
-use App\Http\Controllers\UserController;
-use App\Http\Middleware\SuperAdminMiddleware;
-use Filament\Facades\Filament;
 use Illuminate\Support\Facades\Route;
 
 Route::view('/maps', 'maps');
