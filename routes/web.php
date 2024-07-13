fb8bd1347fadbb66ed28110a74a15f8d432ee5fb
Myhkail Mendoza <myhkail.mendoza@gmail.com>
2024-07-13 19:09:20 +0900

    mail test

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,17 @@
 use App\Http\Middleware\AdminMiddleware;
 use App\Livewire\Homepage;
 use App\Livewire\Termsandconditions;
+use App\Mail\MyTestEmail;
+use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Route;
 
 Route::view('/maps', 'maps');
 Route::get('/', Homepage::class);
 Route::get('/rules', Termsandconditions::class);
+//Route::get('/testroute', Homepage::class);
+
+
+Route::get('/testroute', function () {
+    $name = "Funny Coder";
+    Mail::to('myhkail.mendoza@gmail.com')->send(new MyTestEmail($name));
+});
