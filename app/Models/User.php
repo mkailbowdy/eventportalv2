7a20996bd80f9378e97549251ab77a461197b049
Myhkail Mendoza <myhkail.mendoza@gmail.com>
2024-07-13 17:30:41 +0900

    file

--- app/Models/User.php
+++ app/Models/User.php
@@ -17,6 +17,7 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Support\Facades\Auth;
 use Filament\Infolists\Components\Section as InfolistSection;
+use Illuminate\Support\Facades\Storage;
 
 
 class User extends Authenticatable implements FilamentUser, HasAvatar
@@ -69,9 +70,16 @@ class User extends Authenticatable implements FilamentUser, HasAvatar
         return $this->hasMany(Comment::class);
     }
 
+//    public function getFilamentAvatarUrl(): ?string
+//    {
+//        return $this->avatar_url;
+//    }
+
     public function getFilamentAvatarUrl(): ?string
     {
-        return $this->avatar_url;
+        return $this->avatar_path
+            ? Storage::url($this->avatar_path)
+            : null;
     }
 
     /**
