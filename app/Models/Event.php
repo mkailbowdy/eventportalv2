7d389c4e95a15ae5c427054cd4733ed71d916bd5
Myhkail Mendoza <myhkail.mendoza@gmail.com>
2024-07-14 11:21:35 +0900

    profile pic

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -73,7 +73,7 @@ class Event extends Model implements HasMedia
     {
         return [
             Section::make('Event')
-                ->description(new HtmlString('Please be sure to read <a href="/"><strong>Event Post Guidelines</strong></a> before uploading an event.'))
+                ->description(new HtmlString('Please be sure to read <a href="/"><span class="text-red-500 font-bold underline">Event Post Guidelines</a> before uploading an event.'))
                 ->columns(2)
                 ->schema([
                     TextInput::make('name')
