fbcac3292e44a0eb28a6e0b44e14e4947c35ef5c
Myhkail Mendoza <myhkail.mendoza@gmail.com>
2024-07-14 10:35:09 +0900

    migration updates and registrion form

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,8 @@
 <?php
 
-use App\Http\Middleware\AdminMiddleware;
 use App\Livewire\Homepage;
 use App\Livewire\Termsandconditions;
 use App\Mail\MyTestEmail;
-use Illuminate\Foundation\Auth\EmailVerificationRequest;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Route;
 
@@ -13,7 +10,7 @@ Route::view('/maps', 'maps');
 Route::get('/', Homepage::class);
 Route::get('/rules', Termsandconditions::class);
 
-Route::get('/testroute', function () {
-    $name = "Funny Coder";
-    Mail::to('myhkail.mendoza@gmail.com')->send(new MyTestEmail($name));
-});
+//Route::get('/testroute', function () {
+//    $name = "Funny Coder";
+//    Mail::to('myhkail.mendoza@gmail.com')->send(new MyTestEmail($name));
+//});
