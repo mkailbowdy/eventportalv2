af62a6e1f35b84fc068391abe7dc4f4c53f3abe2
Myhkail Mendoza <myhkail.mendoza@gmail.com>
2024-07-14 10:23:01 +0900

    migration updates and registrion form

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,8 @@ use App\Http\Middleware\AdminMiddleware;
 use App\Livewire\Homepage;
 use App\Livewire\Termsandconditions;
 use App\Mail\MyTestEmail;
+use Illuminate\Foundation\Auth\EmailVerificationRequest;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +19,19 @@ Route::get('/testroute', function () {
     $name = "Funny Coder";
     Mail::to('myhkail.mendoza@gmail.com')->send(new MyTestEmail($name));
 });
+
+Route::get('/email/verify', function () {
+    return view('auth.verify-email');
+})->middleware('auth')->name('verification.notice');
+
+Route::get('/email/verify/{id}/{hash}', function (EmailVerificationRequest $request) {
+    $request->fulfill();
+
+    return redirect('/dashboard');
+})->middleware(['auth', 'signed'])->name('verification.verify');
+
+Route::post('/email/verification-notification', function (Request $request) {
+    $request->user()->sendEmailVerificationNotification();
+
+    return back()->with('message', 'Verification link sent!');
+})->middleware(['auth', 'throttle:6,1'])->name('verification.send');
